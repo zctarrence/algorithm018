7c17afd592829472d33dcee33a58b67dfa5b34df
GeekUniversity <hzc_tarrence@outlook.com>
2020-11-29 22:24:30 +0800

    week06 assignment

---
+++ week06/[221]最大正方形.java
@@ -0,0 +1,44 @@
+//在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。 
+//
+// 
+//
+// 示例： 
+//
+// 
+//输入：
+//matrix = [["1","0","1","0","0"],
+//          ["1","0","1","1","1"],
+//          ["1","1","1","1","1"],
+//          ["1","0","0","1","0"]]
+//
+//输出：4 
+// Related Topics 动态规划 
+// 👍 625 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int maxSide = 0;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return maxSide;
+        }
+        int rows = matrix.length, columns = matrix[0].length;
+        int[][] dp = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    }
+                    maxSide = Math.max(maxSide, dp[i][j]);
+                }
+            }
+        }
+        int maxSquare = maxSide * maxSide;
+        return maxSquare;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ week06/[363]矩形区域不超过 K 的最大数值和.java
@@ -0,0 +1,59 @@
+//给定一个非空二维矩阵 matrix 和一个整数 k，找到这个矩阵内部不大于 k 的最大矩形和。 
+//
+// 示例: 
+//
+// 输入: matrix = [[1,0,1],[0,-2,3]], k = 2
+//输出: 2 
+//解释: 矩形区域 [[0, 1], [-2, 3]] 的数值和是 2，且 2 是不超过 k 的最大数字（k = 2）。
+// 
+//
+// 说明： 
+//
+// 
+// 矩阵内的矩形区域面积必须大于 0。 
+// 如果行数远大于列数，你将如何解答呢？ 
+// 
+// Related Topics 队列 二分查找 动态规划 
+// 👍 137 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxSumSubmatrix(int[][] matrix, int k) {
+        int rows = matrix.length, cols = matrix[0].length, max = Integer.MIN_VALUE;
+
+        for (int l = 0; l < cols; l++) {
+            int[] rowSum = new int[rows];
+            for (int r = l; r < cols; r++) {
+                for (int i = 0; i < rows; i++) {
+                    rowSum[i] += matrix[i][r];
+                }
+                max = Math.max(max, dpmax(rowSum, k));
+                if (max == k) return k;
+            }
+        }
+        return max;
+    }
+
+
+    private int dpmax(int[] arr, int k) {
+        int rollSum = arr[0], rollMax = rollSum;
+        for (int i = 1; i < arr.length; i++) {
+            if (rollSum > 0) rollSum += arr[i];
+            else rollSum = arr[i];
+            if (rollSum > rollMax) rollMax = rollSum;
+        }
+        if (rollMax <= k) return rollMax;
+        int max = Integer.MIN_VALUE;
+        for (int l = 0; l < arr.length; l++) {
+            int sum = 0;
+            for (int r = l; r < arr.length; r++) {
+                sum += arr[r];
+                if (sum > max && sum <= k) max = sum;
+                if (max == k) return k;
+            }
+        }
+        return max;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
